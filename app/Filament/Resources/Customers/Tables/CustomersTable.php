40ec4d1b89c40070d43be2a42351fcecb497d260
Vibol Khan <vibol.khan.mail@gmail.com>
2025-11-17 13:49:52 +0700

    first commit

---
+++ app/Filament/Resources/Customers/Tables/CustomersTable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Resources\Customers\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Table;
+
+class CustomersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
